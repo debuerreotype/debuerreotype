e2b18d9fbda77ef37c6325ccba0b9590042f235a
Tianon Gravi <admwiggin@gmail.com>
2019-02-13 13:39:28 -0800

    Tweak a bit to ensure we're still good when updating our Dockerfile to be FROM buster (newer debootstrap)
    - more aggressive keyring handling (buster's got wheezy's key relegated to the removed-keys keyring now, for example)
    - exclude "/run/shm/.run-transition" (which only shows up when building with buster+ debootstrap, for some reason -- possibly related to ischroot/mount differences?)

--- build.sh
+++ build.sh
@@ -112,17 +112,18 @@ docker run \
 			touch_epoch "$snapshotUrlFile"
 		done
 
-		if [ -z "$eol" ]; then
-			keyring=/usr/share/keyrings/debian-archive-keyring.gpg
+		export GNUPGHOME="$(mktemp -d)"
+		keyring="$GNUPGHOME/debian-archive-$suite-keyring.gpg"
+		if [ "$suite" = potato ]; then
+			# src:debian-archive-keyring was created in 2006, thus does not include a key for potato
+			gpg --batch --no-default-keyring --keyring "$keyring" \
+				--keyserver ha.pool.sks-keyservers.net \
+				--recv-keys 8FD47FF1AA9372C37043DC28AA7DEB7B722F1AED
 		else
-			keyring=/usr/share/keyrings/debian-archive-removed-keys.gpg
-
-			if [ "$suite" = potato ]; then
-				# src:debian-archive-keyring was created in 2006, thus does not include a key for potato
-				export GNUPGHOME="$(mktemp -d)"
-				keyring="$GNUPGHOME/debian-archive-$suite-keyring.gpg"
-				gpg --no-default-keyring --keyring "$keyring" --keyserver ha.pool.sks-keyservers.net --recv-keys 8FD47FF1AA9372C37043DC28AA7DEB7B722F1AED
-			fi
+			# check against all releases (ie, combine both "debian-archive-keyring.gpg" and "debian-archive-removed-keys.gpg"), since we cannot really know whether the target release became EOL later than the snapshot date we are targeting
+			gpg --batch --no-default-keyring --keyring "$keyring" --import \
+				/usr/share/keyrings/debian-archive-keyring.gpg \
+				/usr/share/keyrings/debian-archive-removed-keys.gpg
 		fi
 
 		snapshotUrl="$(< "$exportDir/$serial/$dpkgArch/snapshot-url")"
