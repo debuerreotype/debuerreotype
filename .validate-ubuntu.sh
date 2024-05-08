b81dcb8699800c34793b77c7d78651a5810a912c
You-Sheng Yang <vicamo@gmail.com>
2024-05-08 20:13:10 +0800

    Install ubuntu-keying in Dockerfile
    Don't create a separate docker image for ubuntu only. Let one image
    rules all.
    
    Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

--- .validate-ubuntu.sh
+++ .validate-ubuntu.sh
@@ -7,24 +7,9 @@ if [ -n "${ARCH:-}" ]; then
 fi
 buildArgs+=( validate "$SUITE" )
 
-dockerImage="$(./.docker-image.sh)"
-dockerImage+='-ubuntu'
-{
-	cat Dockerfile - <<-'EODF'
-		RUN set -eux; \
-# https://bugs.debian.org/929165 :(
-# http://snapshot.debian.org/package/ubuntu-keyring/
-# http://snapshot.debian.org/package/ubuntu-keyring/2020.06.17.1-1/
-			wget -O ubuntu-keyring.deb 'http://snapshot.debian.org/archive/debian/20210307T083530Z/pool/main/u/ubuntu-keyring/ubuntu-keyring_2020.06.17.1-1_all.deb'; \
-			echo 'c2d8c4a9be6244bbea80c2e0e7624cbd3a2006a2 *ubuntu-keyring.deb' | sha1sum --strict --check -; \
-			apt-get install -y --no-install-recommends ./ubuntu-keyring.deb; \
-			rm ubuntu-keyring.deb
-	EODF
-} | docker build --pull --tag "$dockerImage" --file - .
-
 mkdir -p validate
 
 set -x
 
 ./scripts/debuerreotype-version
-./docker-run.sh --image="$dockerImage" --no-build ./examples/ubuntu.sh "${buildArgs[@]}"
+./docker-run.sh --pull ./examples/ubuntu.sh "${buildArgs[@]}"
--- Dockerfile
+++ Dockerfile
@@ -27,6 +27,15 @@ RUN set -eux; \
 ENV WGETRC /.wgetrc
 RUN echo 'hsts=0' >> "$WGETRC"
 
+# https://bugs.debian.org/929165 :(
+# http://snapshot.debian.org/package/ubuntu-keyring/
+# http://snapshot.debian.org/package/ubuntu-keyring/2020.06.17.1-1/
+RUN set -eux; \
+	wget -O ubuntu-keyring.deb 'http://snapshot.debian.org/archive/debian/20210307T083530Z/pool/main/u/ubuntu-keyring/ubuntu-keyring_2020.06.17.1-1_all.deb'; \
+	echo 'c2d8c4a9be6244bbea80c2e0e7624cbd3a2006a2 *ubuntu-keyring.deb' | sha1sum --strict --check -; \
+	apt-get install -y --no-install-recommends ./ubuntu-keyring.deb; \
+	rm ubuntu-keyring.deb
+
 # https://github.com/debuerreotype/debuerreotype/issues/100
 # https://tracker.debian.org/pkg/distro-info-data
 # http://snapshot.debian.org/package/distro-info-data/
